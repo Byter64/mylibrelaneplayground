48495879652b17c637fea0aa4efb456d7c9cd20b
Byter <byter64@gmx.de>
2026-01-23 12:51:08 +0100

    Replaced: SRAM with 2Px16 bit SRAM Fixed: Behavioural model of SRAM did not synthesise with yosys (used real type)

--- src/Framebuffer.v
+++ src/Framebuffer.v
@@ -7,10 +7,10 @@ module Framebuffer (
     input writeEnableA,
     output reg[15:0] dataOutA,
 
-    (* keep *)input clkB,
-    (* keep *)input[15:0] dataInB,
-    (* keep *)input[16:0] addressB,
-    (* keep *)input writeEnableB,
+    input clkB,
+    input[15:0] dataInB,
+    input[16:0] addressB,
+    input writeEnableB,
     output reg[15:0] dataOutB
 );
  
@@ -18,9 +18,7 @@ module Framebuffer (
 //Ein cooler start-up screen ist glaub ich nicht im Rahmen der Möglichkeiten :(
 //initial $readmemh("C:/Repos/Hans2/HardwareDesign/Graphicsystem/StartScreen.hex", memory);
 
-assign dataOutB = dataOutA;
-
-RM_IHPSG13_1P_1024x32_c2_bm_bist sram0 (
+RM_IHPSG13_2P_1024x16_c2_bm_bist sram0 (
     .A_CLK  (clkA),
     .A_MEN  (1'b1),
     .A_WEN  (writeEnableA & (addressA[16:10] == 0)),
@@ -31,6 +29,16 @@ RM_IHPSG13_1P_1024x32_c2_bm_bist sram0 (
     .A_DOUT (dataOutA),
     .A_BM   ({16{1'b1}}),
 
+    .B_CLK  (clkB),
+    .B_MEN  (1'b1),
+    .B_WEN  (writeEnableB & (addressB[16:10] == 0)),
+    .B_REN  (!writeEnableB & (addressB[16:10] == 0)),
+    .B_ADDR (addressB[9:0]),
+    .B_DIN  (dataInB),
+    .B_DLY  (1'b1), // tie high!
+    .B_DOUT (dataOutB),
+    .B_BM   ({16{1'b1}}),
+
     // Built-in self test port
     .A_BIST_CLK   ('0),
     .A_BIST_EN    ('0),
@@ -39,7 +47,16 @@ RM_IHPSG13_1P_1024x32_c2_bm_bist sram0 (
     .A_BIST_REN   ('0),
     .A_BIST_ADDR  ('0),
     .A_BIST_DIN   ('0),
-    .A_BIST_BM    ('0)
+    .A_BIST_BM    ('0),
+
+    .B_BIST_CLK   ('0),
+    .B_BIST_EN    ('0),
+    .B_BIST_MEN   ('0),
+    .B_BIST_WEN   ('0),
+    .B_BIST_REN   ('0),
+    .B_BIST_ADDR  ('0),
+    .B_BIST_DIN   ('0),
+    .B_BIST_BM    ('0)
 );
 
 /* Fucking generate scheint nicht mit librelane zu funktionieren. Die [xx] Namensgebung macht Probleme.
